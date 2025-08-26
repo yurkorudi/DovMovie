84f12997de7fb94d061210079afc41d22a6e3ec8
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2025-08-26 15:19:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -790,7 +790,7 @@ def cash_prod():
     data  = {
     "ver": 6,
     "source": "DM_API",
-    "device": "test",
+    "device": {app.config['DM_DEVICE']},
     "tag": "",
     "need_pf_img": "0",
     "need_pf_pdf": "0",
