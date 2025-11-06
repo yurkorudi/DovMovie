c2aaa1720a6383de0a68d344ce3337490c23a6ab
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2025-11-06 17:55:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1183,7 +1183,8 @@ def liqpay(movie_data=None, selected_seats=None):
 
         "result_url": f"http://178.62.106.58/success_loading?order_id={order_id}",
 
-        "server_url": f"http://178.62.106.58/payment_callback?order_id={order_id}"
+        "server_url": f"http://178.62.106.58/payment_callback?order_id={order_id}",
+        "sandbox": 1
     }
         
         
@@ -1344,7 +1345,7 @@ def payment_callback():
         try:
             print("\n \n \n \n Sending ticket email...")
             pdf_bytes = url_for('ticket_pdf', order_id=order_id)
-            send_ticket_to_mail(payment.email, pdf_bytes, user_inf['movie_title'], '15:30')
+            send_ticket_to_mail(payment.email, pdf_bytes, confirmation_data['movie_title'], '15:30')
         except Exception as e:
             print("Error sending ticket email:", e)
         
@@ -1408,8 +1409,11 @@ def payment_callback():
                     ],
                     "pays": [
                         {
-                            "type": 0,
+                            "type": 17,
                             "sum": sum,
+                            "change": 0.00,
+                            "comment": {comments},
+                            "currency": payload.get("currency")
                         }
                     ]
                 }
@@ -1423,10 +1427,13 @@ def payment_callback():
             print("\n \n \n \n \n _______________________________ rror printing receipt:", e)
        
        
+    else:
+        Ticket.query.filter_by(order_id=order_id).delete()
+        db.session.commit()
+       
        
        
     return jsonify({
-        'status': 'success',
         'message': 'Payment successful',
         'order_id': order_id,
         'payment_status': payment.status
@@ -1454,11 +1461,7 @@ def success():
             print(f"Помилка обробки даних: {e}")
             user_inf = None
             
-    if success_pay == 'True':
-        pass
-    else:
-        Ticket.query.filter_by(order_id=order_id).delete()
-        db.session.commit()
+
 
     
     return render_template(
