e051faf0d77af8a3ea373b1f9e07caa8893946d3
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2025-11-18 18:11:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1501,17 +1501,20 @@ def payment_callback():
             print(f"Помилка обробки даних: {e}")
             user_inf = "error"
             
-        
+    print("\n \n \n \n \n \n \n \n entering if \n \n \n \n \n )
         
     if status == "success" or status == "sandbox":
+        
         try:
-            print("\n \n \n \n Sending ticket email...")
+            print("\n \n \n \n Sending ticket email...\n \n \n \n ")
             pdf_bytes = url_for('ticket_pdf', order_id=order_id)
             send_dovzhenko_ticket_email(
         recipient=payment.email,
         movie_title=confirmation_data['movie'],
-        session_datetime=Showtime.query.filter_by(id=payment.sessionId).first().dateTime.strftime('%d.%m %H:%M'),
+        session_datetime='15^40 20.12',
+        # session_datetime=Showtime.query.filter_by(id=payment.sessionId).first().dateTime.strftime('%d.%m %H:%M'),
         pdf_bytes=pdf_bytes)
+            print("\n \n \n \n \n \n \n \n Ticket email sent.\n \n \n \n \n \n \n \n ")
         except Exception as e:
             print("\n \n \n Error sending ticket email: \n \n \n ", e)
         
