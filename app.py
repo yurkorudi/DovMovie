2ed21046fb4dd702156a401270803490a9b58523
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2026-08-05 21:50:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,8 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['DM_DEVICE'] = 'kasar'
 app.config['DM_ONLINE_DEVICE'] = 'kasar_online'
 app.config['ADMIN_PASSWORD'] = os.environ.get('ADMIN_PASSWORD', 'DovzhenkoAdminPassword')
-LIQPAY_PUBLIC_KEY = 'sandbox_i65007373353'
-LIQPAY_PRIVATE_KEY = 'sandbox_PiK0U7MpSJ69BAmzqIDS38ypRYJYNbDK9Oi2tt4M'
+LIQPAY_PUBLIC_KEY = 'i40470776966'
+LIQPAY_PRIVATE_KEY = 'mHLYgc7FLwKeqBrpp6Pay4O7a4GBr9gueYdJLeKB'
 EMAIL = 'lviv.dovzhenkocentre@gmail.com'
 PASSWORD = 'ypgg dhnr aubi kzyi'
 
